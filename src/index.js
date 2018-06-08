7220ccad21c0a381b9f730bf262fa6fc45e6fa92
gigarthan <gigajpx@gmail.com>
2018-06-08 21:04:45 +0530

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { configStore } from './store/index';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const state = {};
+
+const store = configStore(state);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+     document.getElementById('root')
+    );
+registerServiceWorker();
