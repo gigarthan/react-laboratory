71755a540d04f0c1950a41eb1e265b7948925336
gigarthan <gigajpx@gmail.com>
2018-06-09 11:37:44 +0530

    setup main wrapper

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import MainWrapper from "containers/MainWrapper";
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  handleChange = key => (event, value) => {
+    this.setState({
+      [key]: value,
+    });
+  };
+
+
   render() {
     return (
-      <div className="App">
-        <h1>WELCOME</h1>
-      </div>
+     <Grid container >
+      <Grid item md={12}>
+        <MainWrapper>
+          <h1>WELCOME</h1>
+        </MainWrapper>
+      </Grid>
+    </Grid>
     );
   }
 }
