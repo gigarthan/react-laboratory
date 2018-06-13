4c4094d67350cdc9fcb2de1f15f37665a64149ae
Neminda <nemindawarnaja@gmail.com>
2018-06-13 14:17:03 +0530

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+
 import Grid from '@material-ui/core/Grid';
 
 import MainWrapper from "containers/MainWrapper";
@@ -15,6 +16,8 @@ import LabTestManagerWrapper from './components/OptionsManager/LabTestManagerWra
 import LaboratoryManagerWrapper from './components/OptionsManager/LaboratoryManagerWrapper';
 import SampleCenterManagerWrapper from './components/OptionsManager/SampleCenterManagerWrapper';
 import SpecimenInformationWrapper from './components/LabOrders/SpecimenInformationWrapper';
+
+
 
 class App extends Component {
 
@@ -34,7 +37,7 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route path="/login" component={Login} />       
+          <Route path="/login" component={Login} />
           <Grid container>
             <Grid item md={12}>
               <MainWrapper >
@@ -49,7 +52,7 @@ class App extends Component {
           </Grid>
         </Switch>
       </Router>
-     
+
     );
   }
 }
