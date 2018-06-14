8bef268c2f596c046cb9808e68bb67319df2abf6
Neminda <nemindawarnaja@gmail.com>
2018-06-14 13:13:31 +0530

    setup lab test manager wrapper

--- src/components/OptionsManager/LabTestManagerWrapper.jsx
+++ src/components/OptionsManager/LabTestManagerWrapper.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import LabTestWrapper from './LabTestManager/LabTestWrapper';
 
 export default class LabTestManagerWrapper extends Component {
   render() {
     return (
       <div>
-        
+          <LabTestWrapper/>
       </div>
     )
   }
