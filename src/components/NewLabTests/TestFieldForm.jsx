d6174c182221dab0f77c17e8cfdc0ee4fb1671fe
gigarthan <gigajpx@gmail.com>
2018-06-14 15:39:06 +0530

    comment reat-swipeable import statement

--- src/components/NewLabTests/TestFieldForm.jsx
+++ src/components/NewLabTests/TestFieldForm.jsx
@@ -16,7 +16,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
-import SwipeableViews from 'react-swipeable-views';
+// import SwipeableViews from 'react-swipeable-views';
 //import TestRequestTable from './ViewTest/ViewTestRequestsHome';
 import TestRequestTable from './ViewTest/TestRequestTable';
 import TestRequestTable1 from './ViewTest/TestRequestTable1';
