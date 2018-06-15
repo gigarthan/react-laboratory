43a6a6a8a70b037d0ce639929aba82bac478ace3
IT16113978 <lmadhavirj7512@gmail.com>
2018-06-15 21:59:28 +0530

    labTestField.js

---
+++ src/store/actions/labTestField.js
@@ -0,0 +1,23 @@
+import axios from "axios/index";
+import {ADD_TESTS_FIELDS} from '../types';
+
+const BASE_URL = 'http://localhost:8000/api';
+
+export function addTestsFields(values) {
+    return async dispatch => {
+        const url = BASE_URL + '/addFields';
+        try {
+            console.log('val',values);
+            const res = await axios.post(url, values);
+            dispatch({
+                type: ADD_TESTS_FIELDS,
+                payload: res.data
+            });
+            console.log('added tests fields');
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
