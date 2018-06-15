5ab3d836d293cebaa81f788d8ef63389286b298e
Neminda <nemindawarnaja@gmail.com>
2018-06-15 23:20:03 +0530

    sample center reducers

--- src/store/reducers/reduce_sampleCenters.js
+++ src/store/reducers/reduce_sampleCenters.js
@@ -6,6 +6,7 @@ import { GET_SAMPLE_CENTERS , ADD_SAMPLE_CENTERS} from './../types';
 
 export default function(state = [], action) {
     switch (action.type) {
+
         case GET_SAMPLE_CENTERS:
             return [ ...action.payload];
 
