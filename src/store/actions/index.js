7f9578400a08ffdef7dad7ecbfcb56f50e2732de
IT16113978 <lmadhavirj7512@gmail.com>
2018-06-15 13:58:27 +0530

    index

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
-import {GET_ORDER, GET_ORDERS, GET_SAMPLE_CENTER_TYPES} from '../types';
+
+import {GET_ORDER, GET_ORDERS} from '../types';
+
 import { GET_SAMPLE_CENTERS } from '../types';
 import { GET_LABS } from '../types';
 import { GET_LAB_TESTS } from '../types';
+import {GET_TESTS} from "../types";
+import {GET_BASIC_TESTS} from "../types";
+import { GET_SAMPLE_CENTER_TYPES } from '../types';
+import {GET_NOTIFICATIONS} from '../types';
 
 const BASE_URL = 'http://localhost:8000/api';
 
@@ -112,6 +118,67 @@ export function getLabTests() {
             const res = await axios.get(url);
             dispatch({
                 type: GET_LAB_TESTS,
+                payload: res.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+
+///////////////////// Get Lab test manager ////////////////
+
+
+export function getAddedLabTests() {
+    return async dispatch => {
+        const url = BASE_URL + '/field/getFields';
+
+        try {
+            const res = await axios.get(url);
+            dispatch({
+                type: GET_TESTS,
+                payload: res.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+
+///////////////////// Get Lab test manager ////////////////
+
+
+export function getBasicAddedLabTests() {
+    return async dispatch => {
+        const url = BASE_URL + '/field/test';
+
+        try {
+            const res = await axios.get(url);
+            dispatch({
+                type: GET_BASIC_TESTS,
+                payload: res.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+
+
+///////////////////// Get Notification manager ////////////////
+
+
+export function getNotifications() {
+    return async dispatch => {
+        const url = BASE_URL + '/field/notification';
+
+        try {
+            const res = await axios.get(url);
+            dispatch({
+                type: GET_NOTIFICATIONS,
                 payload: res.data
             });
         } catch (error) {
