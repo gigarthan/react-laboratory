783399effc343dc8d98d89d91d2f57e2ac4aa3cb
Neminda <nemindawarnaja@gmail.com>
2018-06-15 17:47:06 +0530

    set up get and post methods for laboratories

---
+++ src/store/actions/laboratory.js
@@ -0,0 +1,108 @@
+import axios from "axios/index";
+import { GET_LABS, ADD_LABS , GET_LAB_DEPARTMENTS,GET_LAB_TYPES ,ADD_LAB_DEPARTMENTS,ADD_LAB_TYPES} from '../types';
+
+const BASE_URL = 'http://localhost:8000/api';
+
+
+
+///////////////////// Laboratory manager ////////////////
+
+
+export function getLabs() {
+    return async dispatch => {
+        const url = BASE_URL + '/labs';
+
+        try {
+            const res = await axios.get(url);
+            dispatch({
+                type: GET_LABS,
+                payload: res.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+
+export function addLabs(values) {
+    return async dispatch => {
+        const url = BASE_URL + '/labs/' ;
+        try {
+            console.log('val', values);
+            const res = await axios.post(url, values);
+            dispatch({
+                type: ADD_LABS,
+                payload: res.data
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export function getLabDepartments() {
+    return async dispatch => {
+        const url = BASE_URL + '/lab-departments';
+
+        try {
+            const res = await axios.get(url);
+            dispatch({
+                type: GET_LAB_DEPARTMENTS,
+                payload: res.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+export function addLabDepartments(values) {
+    return async dispatch => {
+        const url = BASE_URL + '/lab-departments/' ;
+        try {
+            const res = await axios.post(url, values);
+            dispatch({
+                type: ADD_LAB_DEPARTMENTS,
+                payload: res.data
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export function getLabTypes() {
+    return async dispatch => {
+        const url = BASE_URL + '/lab-types';
+
+        try {
+            const res = await axios.get(url);
+            dispatch({
+                type: GET_LAB_TYPES,
+                payload: res.data
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+
+
+export function addLabTypes(name) {
+    return async dispatch => {
+        const url = BASE_URL + '/lab-types/' ;
+        try {
+            const res = await axios.post(url, { name });
+            dispatch({
+                type: ADD_LAB_TYPES,
+                payload: res.data
+            });
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+}
